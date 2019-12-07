7765384b58e5c05f6bf5143afa432faf11962a4e
Daniel Varcas <daniel.varcas@gmail.com>
2019-12-07 13:08:36 +0000

    Setup bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      <p>Hello, world.</p>
-    </div>
+    <Container>
+      <Row>
+        <Col>Hello, world.</Col>
+      </Row>
+    </Container>
   );
 }
 
