19918b0714b916d58bfbb8ae57829e36a55e4e9f
Daniel Varcas <daniel.varcas@gmail.com>
2019-12-08 12:28:08 +0000

    Play around with some styles and MUI styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,66 @@
 import React from "react";
-import Container from "@material-ui/core/Button";
+import { makeStyles, styled } from "@material-ui/core/styles";
+
+import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import {
-  Typography,
-  AppBar,
-  Toolbar,
-  Button,
-  Paper,
-  Card,
-  CardActions,
-  CardContent
-} from "@material-ui/core";
+import { Typography, AppBar, Toolbar, Button, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(3),
+    margin: theme.spacing(3)
+  },
+  myButton: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2)
+  }
+}));
+
+const SiteAppBar = styled(AppBar)({
+  background: "linear-gradient(45deg, #0F2027, #203A43, #2c5364)",
+  color: "white"
+});
+
+const MyButton = styled(Button)({
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+});
 
 function App() {
+  const classes = useStyles();
+
+  const numbers = [1, 2, 3, 4, 5];
+  const mappedItems = numbers.map(number => (
+    <Grid item xs={12} sm={6}>
+      <Paper className={classes.paper}>{number}</Paper>
+    </Grid>
+  ));
+
   return (
     <>
-      <AppBar position="static">
+      <SiteAppBar position="static">
         <Toolbar>
           <Typography variant="h6">DV</Typography>
-        </Toolbar>
-      </AppBar>
+          {[1, 2, 3].map(() => (
+            <MyButton className={classes.myButton}>Hello</MyButton>
+          ))}
 
-      <Grid container>
-        <Grid item xs={12}>
-          <Paper>
-            <Typography variant="h1">Hello, world.</Typography>
-          </Paper>
+          <Button color="inherit">Hi</Button>
+        </Toolbar>
+      </SiteAppBar>
+
+      <Container>
+        <Grid container className={classes.root}>
+          <Grid item xs={12}>
+            <Typography variant="h1" align="center">
+              Hello, world.
+            </Typography>
+            <Typography variant="h2" align="center">
+              Welcome to my website.
+            </Typography>
+          </Grid>
+          {mappedItems}
         </Grid>
-      </Grid>
+      </Container>
     </>
   );
 }
