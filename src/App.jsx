23c2d4cdb0326120e367f288c9d375709a2fa43d
Daniel Varcas <daniel.varcas@gmail.com>
2019-12-08 22:41:23 +0000

    Experiment with colours, buttons, appbar. Add FontAwesome.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import { makeStyles, styled } from "@material-ui/core/styles";
 
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import { Typography, AppBar, Toolbar, Button, Paper } from "@material-ui/core";
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  IconButton
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const instagramGradient = "linear-gradient(90deg, #833ab4, #fd1d1d, #fcb045)";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -11,54 +21,65 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(3)
   },
   myButton: {
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2)
+    background: instagramGradient,
+    color: "white"
+  },
+  appBarButton: {
+    marginLeft: theme.spacing(3)
+  },
+  appBar: {},
+  appBarHeader: {
+    textTransform: "uppercase",
+    flexGrow: 1
+  },
+  menuIcon: {
+    color: "white"
   }
 }));
 
 const SiteAppBar = styled(AppBar)({
-  background: "linear-gradient(45deg, #0F2027, #203A43, #2c5364)",
+  background: "black",
   color: "white"
-});
-
-const MyButton = styled(Button)({
-  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
 });
 
 function App() {
   const classes = useStyles();
 
-  const numbers = [1, 2, 3, 4, 5];
-  const mappedItems = numbers.map(number => (
-    <Grid item xs={12} sm={6}>
-      <Paper className={classes.paper}>{number}</Paper>
-    </Grid>
+  const links = [
+    { text: "GitHub", icon: "fab fa-github" },
+    { text: "LinkedIn", icon: "" },
+    { text: "Twitter", icon: "" }
+  ];
+  const linkButtons = links.map(link => (
+    <Button className={[classes.myButton, classes.appBarButton]}>
+      <i className={link.icon} />
+      {link.text}
+    </Button>
   ));
 
   return (
     <>
-      <SiteAppBar position="static">
+      <SiteAppBar className={classes.appBar} position="static">
         <Toolbar>
-          <Typography variant="h6">DV</Typography>
-          {[1, 2, 3].map(() => (
-            <MyButton className={classes.myButton}>Hello</MyButton>
-          ))}
-
-          <Button color="inherit">Hi</Button>
+          <IconButton>
+            <MenuIcon className={classes.menuIcon} />
+          </IconButton>
+          <Typography className={classes.appBarHeader} variant="h6">
+            Daniel Varcas
+          </Typography>
+          {linkButtons}
         </Toolbar>
       </SiteAppBar>
 
       <Container>
         <Grid container className={classes.root}>
           <Grid item xs={12}>
-            <Typography variant="h1" align="center">
-              Hello, world.
-            </Typography>
-            <Typography variant="h2" align="center">
-              Welcome to my website.
-            </Typography>
+            <Box margin={2}>
+              <Typography variant="h1">Hello, world.</Typography>
+              <Typography variant="h2">Welcome to my website.</Typography>
+            </Box>
           </Grid>
-          {mappedItems}
+          {linkButtons}
         </Grid>
       </Container>
     </>
